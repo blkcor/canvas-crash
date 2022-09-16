6c1c5dd3e980acb661da0b5dacccf117dceb58ad
blkcor <1029129867@qq.com>
2022-09-16 14:02:40 +0800

    remove

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 window.addEventListener('load', () => {
     const canvas = document.querySelector('#canvas1');
-    const context = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    //initial style  setting
-    context.fillStyle = 'red';
-    context.strokeStyle = 'yellow';
-    context.lineCap = 'round';
-    context.lineWidth = 30;
-    context.fillRect(50, 50, 100, 100);
-
-    context.beginPath();
-    context.moveTo(100, 100);
-    context.lineTo(300, 300);
-    context.stroke();
+    const gradient1 = ctx.createLinearGradient(10, 10, 400, 10);
+    gradient1.addColorStop(0, "#00ff00");
+    gradient1.addColorStop(1, "#ff0000");
+    const gradient2 = ctx.createLinearGradient(10, 10, 400, 10);
+    // 从0.5的位置才开始渐变
+    gradient2.addColorStop(0.5, "#00ff00");
+    gradient2.addColorStop(1, "#ff0000");
+    ctx.beginPath()
+    ctx.fillStyle = gradient1;
+    ctx.fillRect(10, 10, 400, 100);
+    ctx.beginPath();
+    ctx.fillStyle = gradient2;
+    ctx.fillRect(10, 150, 400, 100);
 })
